b7464b388f300fbbcb4e05dd69759851df6c83eb
Udlei Nati <udlei@protonmail.ch>
2017-01-05 05:11:08 +0100

    Adjusting celery

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,7 @@
 set -e
 
 if [ "$1" = '' ]; then
-    celery worker --workdir /app --concurrency=10 -n worker-$ENVIRONMENT-1@$GITHUB_REVISION --loglevel=INFO \
-    	--without-gossip --without-mingle --without-heartbeat
+    celery worker --workdir /app --concurrency=10 -n worker-$ENVIRONMENT-1@$GITHUB_REVISION --loglevel=INFO
 fi
 
 exec "$@"
