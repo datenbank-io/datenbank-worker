d43adc4877a2c75218cb001c8ad3e69cc56f75fe
Udlei Nati <udlei@protonmail.ch>
2017-01-05 03:16:09 +0100

    Deploying to ECS

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "$1" = '' ]; then
-    celery worker --workdir /app --loglevel=INFO
+    celery worker --workdir /app --concurrency=10 -n worker-$ENVIRONMENT-1@$GITHUB_REVISION --loglevel=INFO 
 fi
 
 exec "$@"
