563dc3e84bab4a8b428ab44528e6d74a1062c1ce
Udlei Nati <udlei@protonmail.ch>
2017-01-05 13:07:13 +0100

    Development tag add to task family

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 eval "echo \"$(<ecs-definition.json)\"" > ecs-definition.tmp.json
 aws ecs register-task-definition --cli-input-json file://ecs-definition.tmp.json > output.json
 export TASK_REVISION=`cat output.json | grep revision | awk '{print $2}'`
-aws ecs update-service --cluster datenbank-worker-$CIRCLE_BRANCH --service worker --task-definition datenbank-worker:$TASK_REVISION
+aws ecs update-service --cluster datenbank-worker-$CIRCLE_BRANCH --service worker \
+	--task-definition datenbank-worker-$CIRCLE_BRANCH:$TASK_REVISION
