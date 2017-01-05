2ac5724bfc09fc727442e18739c3fc3bc287758e
Udlei Nati <udlei@protonmail.ch>
2017-01-05 04:27:59 +0100

    CloudAMQP with Celery
    https://www.cloudamqp.com/docs/celery.html

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,3 +2,9 @@ import os
 BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'amqp://')
 CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'rpc://')
 CELERY_IMPORTS = ('helloworld-worker', )
+
+BROKER_POOL_LIMIT = 1 # Will decrease connection usage
+BROKER_HEARTBEAT = None # We're using TCP keep-alive instead
+BROKER_CONNECTION_TIMEOUT = 30 # May require a long timeout due to Linux DNS timeouts etc
+CELERY_SEND_EVENTS = False # Will not create celeryev.* queues
+CELERY_EVENT_QUEUE_EXPIRES = 60 # Will delete all celeryev. queues without consumers after 1 minute.
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,8 @@
 set -e
 
 if [ "$1" = '' ]; then
-    celery worker --workdir /app --concurrency=10 -n worker-$ENVIRONMENT-1@$GITHUB_REVISION --loglevel=INFO 
+    celery worker --workdir /app --concurrency=10 -n worker-$ENVIRONMENT-1@$GITHUB_REVISION --loglevel=INFO \
+    	--without-gossip --without-mingle --without-heartbeat
 fi
 
 exec "$@"
